4ffb21fde6de5ada4714131f2d8fd0abc6f22689
Lekeaka Joe <lekeakajoe@gmail.com>
2022-12-11 11:03:13 +0100

    save

---
+++ src/modules/voter/components/displayPost.tsx
@@ -0,0 +1,33 @@
+import { FaAngleRight } from 'react-icons/fa';
+import { CandidateEntity } from 'server/modules/candidate/entities/candidate.entity';
+import { CandidateCard } from './candidateCard';
+
+export const DisplayPost = ({
+  post,
+  candidates,
+}: {
+  post: string;
+  candidates: CandidateEntity[];
+}) => {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <h2 style={{ marginLeft: 5 }}>
+        <FaAngleRight
+          color='red'
+          style={{ marginBottom: -3, marginRight: 5 }}
+        />
+        {post}
+      </h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {candidates.map((candidate) => (
+          <CandidateCard key={candidate._id} candidate={candidate} />
+        ))}
+        {candidates.length == 0 && (
+          <h4 style={{ color: 'red', textAlign: 'center' }}>
+            No registered candidate
+          </h4>
+        )}
+      </div>
+    </div>
+  );
+};
