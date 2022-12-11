e8470416aef61e0273420f79019e9ab5583e29d1
Lekeaka Joe <lekeakajoe@gmail.com>
2022-12-11 19:35:54 +0100

    modification made

--- src/modules/voter/components/registrationForm.tsx
+++ src/modules/voter/components/registrationForm.tsx
@@ -31,7 +31,7 @@ export const RegistrationForm = () => {
             signIn('credentials');
           }
           setIsLoading(false);
-        }, 10000);
+        }, 13000);
         setIsLoading(true);
         // const profilePicture = data.upload?.fileList[0]?.originFileObj;
         // const postData = { ...data, image: profilePicture } as VoterEntity;
